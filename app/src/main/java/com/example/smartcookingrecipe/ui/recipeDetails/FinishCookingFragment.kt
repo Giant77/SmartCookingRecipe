90a287b43d0580ac8614cb937ca292968f4cdadd
Giant77 <willyarsyad77@gmail.com>
2025-05-30 14:43:44 +0700

    feat: enhance inventory management UI with unit selection and expiry date picker

--- app/src/main/java/com/example/smartcookingrecipe/ui/recipeDetails/FinishCookingFragment.kt
+++ app/src/main/java/com/example/smartcookingrecipe/ui/recipeDetails/FinishCookingFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import com.example.smartcookingrecipe.R
 import com.example.smartcookingrecipe.databinding.FragmentPostCookingBinding
 
 
@@ -23,13 +26,17 @@ class FinishCookingFragment : Fragment() {
         _binding = FragmentPostCookingBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-//        binding.btnBack.setOnClickListener {
-//            findNavController().popBackStack() // Close this page
-//        }
-//
-//        binding.btnProceedToSteps.setOnClickListener {
-//            findNavController().popBackStack() // Close this page
-//        }
+        val navOptions = NavOptions.Builder()
+            .setPopUpTo(findNavController().graph.startDestinationId, false) // Clears the stack up to this fragment
+            .build()
+
+        binding.btnGoHome.setOnClickListener {
+            findNavController().navigate(R.id.act_postCooking_to_home, null, navOptions)
+        }
+
+        binding.btnCheckNutrition.setOnClickListener {
+            findNavController().navigate(R.id.act_postCooking_to_nutrition, null, navOptions)
+        }
 
         return root
     }
